f15b2c5ef6af7599a7283abc120e2ef5a87198e3
Onam Chilwan <OnamC@asos.com>
2020-12-17 11:30:31 +0000

    STORY 1 modelled gift aid as a value type too. Arguably can make constructor except a tax rate but we don't have a requirement to use multiple tax rates.
    Everything is still in unit test project. Doing the bare minimum to get test passing.

--- JG.FinTechTest.Tests/GiftAidCalculatorTests.cs
+++ JG.FinTechTest.Tests/GiftAidCalculatorTests.cs
@@ -1,5 +1,4 @@
 using NUnit.Framework;
-using NUnit.Framework.Constraints;
 
 namespace JG.FinTechTest.Tests
 {
@@ -11,22 +10,30 @@ namespace JG.FinTechTest.Tests
         [TestCase(1234, 308.5)]
         public void Given_A_Donation_When_Calculating_Gift_Aid_The_Correct_Amount_Is_Returned(decimal donationAmount, decimal expectedGiftAidAmount)
         {
-            var subject = new GiftAidCalculator();
-            var result = subject.Calculate(donationAmount);
-
-            Assert.That(result, Is.EqualTo(expectedGiftAidAmount));
+            var subject = new GiftAid(donationAmount);
+            Assert.That(subject.Amount, Is.EqualTo(expectedGiftAidAmount));
         }
     }
 
-    public class GiftAidCalculator
+    public class GiftAid
     {
-        public decimal Calculate(decimal donationAmount)
+        private readonly decimal _donation;
+
+        public GiftAid(decimal donation)
+        {
+            _donation = donation;
+        }
+
+        private decimal Calculate()
         {
             var tax = new BasicTaxRate();
             var giftAidRatio = tax.Rate / (100 - tax.Rate);
-            return donationAmount * giftAidRatio;
+            return _donation * giftAidRatio;
         }
+
+        public decimal Amount => Calculate();
     }
+    
 
     public class BasicTaxRate
     {
