e92bcf5bfbb553e256fe9f966064f69216ec829e
Onam Chilwan <OnamC@asos.com>
2020-12-17 12:04:56 +0000

    STORY 2 Get all the scaffolding in for bddfy and create first test to verify newly created route. This would usually be a failing test but the controller was already added. Next commit will be the failing test to ensure the output response is correct.
    Again, unit test not split out as I am doing minimum amount of work to get going and refactor towards the end.

---
+++ JG.FinTechTest.Tests/Controllers/GiftAidControllerTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using NUnit.Framework;
+using TestStack.BDDfy;
+
+namespace JG.FinTechTest.Tests.Controllers
+{
+    [TestFixture]
+    public class GiftAidControllerTests
+    {
+        [Test]
+        public void SuccessfullyRetrievingGiftAidFromDonation()
+        {
+            new GiftAidSteps()
+                .Given(x => x.GivenADonationOf(12.34m))
+                .When(x => x.WhenRequestIsSentToCalculateGiftAid())
+                .Then(x => x.ThenAnOkayResponseIsReturned());
+        }
+    }
+
+    internal class GiftAidSteps
+    {
+        private readonly HttpClient _httpClient;
+        private HttpResponseMessage _httpResponse;
+        private decimal _donation;
+
+        public GiftAidSteps()
+        {
+            _httpClient = TestBuilder.CreateFakeHttpClient();
+        }
+
+        public void GivenADonationOf(decimal donation)
+        {
+            _donation = donation;
+        }
+
+        public void WhenRequestIsSentToCalculateGiftAid()
+        {
+            _httpResponse = _httpClient.GetAsync($"/api/giftaid/{_donation}", CancellationToken.None).GetAwaiter().GetResult();
+        }
+
+        public void ThenAnOkayResponseIsReturned()
+        {
+            Assert.That(_httpResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+    }
+
+    internal class TestBuilder
+    {
+        public static HttpClient CreateFakeHttpClient()
+        {
+            var webhostBuilder = new WebHostBuilder().UseStartup<Startup>();
+            var fakeHttpClient = new TestServer(webhostBuilder).CreateClient();
+            return fakeHttpClient;
+        }
+    }
+}
--- JG.FinTechTest/Startup.cs
+++ JG.FinTechTest/Startup.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace JG.FinTechTest
 {
@@ -22,13 +14,11 @@ namespace JG.FinTechTest
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -41,7 +31,8 @@ namespace JG.FinTechTest
             }
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseRouting();
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
